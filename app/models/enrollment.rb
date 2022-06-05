73729227ba3fef9965cf4be55b67f85d76278afe
watilacosta <watilafreire26@gmail.com>
2022-06-05 11:17:19 -0300

    created errolment resources and yours spec validations

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 class Enrollment < ApplicationRecord
   belongs_to :student
+
+  validates :amount, presence: true
+  validates :amount, numericality: { greater_than: 0 }
+  validates :installments, presence: true
+  validates :installments, numericality: { greater_than: 1 }
+  validates :due_day, presence: true
+  validates_numericality_of :due_day, greater_than_or_equal_to: 1,
+                                      less_than_or_equal_to: 31
 end
--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Enrollment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:amount) }
+    it { is_expected.to validate_numericality_of(:amount).is_greater_than(0) }
+    it { is_expected.to validate_presence_of(:installments) }
+    it { is_expected.to validate_numericality_of(:installments).is_greater_than(1) }
+    it { is_expected.to validate_presence_of(:due_day) }
+    it do
+      is_expected.to validate_numericality_of(:due_day)
+        .is_greater_than_or_equal_to(1)
+        .is_less_than_or_equal_to(31)
+    end
+  end
 end
