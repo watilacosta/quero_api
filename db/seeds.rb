9cd49437de140f0faf71155305a682f6ab8294eb
watila <watilafreire26@gmail.com>
2022-06-07 20:23:49 -0300

    populated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@ puts 'Creating Faker Students'
 end
 
 puts "Created #{Student.count} students"
+
+Student.first(10).each do |student|
+  puts "Creating #{student.name} enrollment"
+  enrollment = FactoryBot.create(:enrollment, student: student)
+
+  enrollment[:installments].times do |index|
+    puts "Creating #{student.name}'s installment's bills"
+
+    bill = Bill.create!(
+      enrollment_id: enrollment[:id],
+      amount: enrollment[:amount] / enrollment[:installments],
+      due_date: if enrollment[:due_day] < Date.today.day
+                  Date.new(Date.today.year, Date.today.month, enrollment[:due_day])
+                      .advance(months: index + 1)
+                else
+                  Date.new(Date.today.year, Date.today.month,
+                           Bill.due_day_validated(enrollment[:due_day])) + index.months
+                end
+    )
+
+    puts "Created bill id = #{bill.inspect}"
+  end
+end
