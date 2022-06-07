8b746f30b72139d7e90e16e1ae0d757ad9d33773
watila <watilafreire26@gmail.com>
2022-06-07 18:07:44 -0300

    refact and delete unuser files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :bills
-  resources :enrollments
+  # resources :bills
+  resources :enrollments, only: %i[index create]
   resources :students, only: %i[index create]
 end
